67be2c3a767f5d4114a5c0a69a6142ab0689a176
Nick-PalPark <nick@willhoft.co.uk>
2025-07-24 15:01:16 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://nick-palpark.github.io',
+  site: 'https://nick-palpark.github.io/my-blog-2/',
   base: '/my-blog-2',
   output: 'static',
 });
