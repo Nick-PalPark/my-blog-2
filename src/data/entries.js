e95f6aea2fde07a50662ac316c84bd5022776111
Nick-PalPark <nick@willhoft.co.uk>
2025-08-11 23:04:54 +0000

    Fix diary card links

--- src/data/entries.js
+++ src/data/entries.js
@@ -1,24 +1,26 @@
-export default [
+const entries = [
   {
-    image: "/images/Totowah_Ttoekbokki and seafood bibimbap.jpg",
+    image: "/images/Ttoekbokki.jpg",
     date: "WED 23RD JUL '25",
     title: "Towering Balsam",
     excerpt: "IT’S BEEN QUITE A WHILE SINCE I WAS OUT with Stroud Valleys Project, so it was nice to catch up with friends...",
-    url: "/posts/towering-balsam"
+    url: "/my-blog-2/posts/towering-balsam"
   },
   {
     image: "/images/harvest.jpg",
     date: "THU 10TH JUL '25",
     title: "Freshford Harvest",
     excerpt: "MY FIRST SEED HARVEST OF THE SEASON with Glorious Grasslands was a long way down. At the very bottom of the Cotswolds...",
-    url: "/posts/freshford-harvest"
+    url: "/my-blog-2/posts/freshford-harvest"
   },
   {
     image: "/images/heritage.jpg",
     date: "WED 9TH JUL '25",
     title: "Heritage Heroes",
     excerpt: "A MID MORNING APPOINTMENT AT THE council chambers at Ebley where the students at the Star College were presented with their awards...",
-    url: "/posts/heritage-heroes"
+    url: "/my-blog-2/posts/heritage-heroes"
   }
-  // Add more entries as needed
 ];
+
+export default entries;
+
