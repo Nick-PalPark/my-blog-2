704f4ecee464e25813e582b41952ac57a034d3fb
Nick-PalPark <nick@willhoft.co.uk>
2025-08-12 15:23:01 +0000

    Test typo for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://nick-palpark.github.io/my-blog-2/',
-  base: '/my-blog-2',
+  base: '/my-blog-2/',
   output: 'static',
 });
