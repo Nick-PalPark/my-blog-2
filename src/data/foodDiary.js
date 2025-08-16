049b550f08f9aedde9b7055aa35a0615f83ba426
Nick-PalPark <nick@willhoft.co.uk>
2025-08-16 23:21:41 +0000

    Add new food diary entry

--- src/data/foodDiary.js
+++ src/data/foodDiary.js
@@ -3,11 +3,18 @@ const foodDiary = [
     date: "2025-07-15",
     restaurant: "산들애건강밥상",
     dish: "Ttoekbokki",
-    image: "/my-blog-2/public/images/Ttoekbokki.jpg",
-    excerpt: "Delicious healthy food.  You need to order when you get a ticket to wait in line so be organized.  Wasn't a long wait.  Lots of side dishes and a really succulent spicy octopus.  The potato pancake is their thing and is delicious and easy to eat a lot of!",
+    image: "/my-blog-2/images/Ttoekbokki.jpg",
+    excerpt: "Delicious healthy food. You need to order when you get a ticket to wait in line; lots of tasty side dishes and a succulent spicy octopus.",
     googleMapsUrl: "https://maps.app.goo.gl/91GRFNrWhoKfuD9v6"
   },
-
+  {
+    date: "2025-07-20",
+    restaurant: "Kimchi House",
+    dish: "Kimchi Jjigae",
+    image: "/my-blog-2/images/Ttoekbokki.jpg",
+    excerpt: "Comforting and spicy stew — perfect on a rainy day.",
+    googleMapsUrl: "https://maps.app.goo.gl/vJTn7ZQ5NpMqzGY16"
+  }
 ];
 
 export default foodDiary;
