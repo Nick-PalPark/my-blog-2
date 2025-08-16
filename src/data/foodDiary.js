b6fc821c8419210b884e28445579c210d23998d4
Nick-PalPark <nick@willhoft.co.uk>
2025-08-16 15:09:08 +0000

    Google integratiuon

---
+++ src/data/foodDiary.js
@@ -0,0 +1,13 @@
+const foodDiary = [
+  {
+    date: "2025-07-15",
+    restaurant: "Seoul Garden",
+    dish: "Bibimbap",
+    image: "/my-blog-2/images/seoul-garden-bibimbap.jpg",
+    excerpt: "Delicious bibimbap with fresh veggies and spicy gochujang.",
+    googleMapsUrl: "https://goo.gl/maps/your-google-maps-link"
+  },
+  // Add more entries here
+];
+
+export default foodDiary;
