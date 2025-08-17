2df6b70b4ff200bfd912842e07204b679f449e1c
Nick-PalPark <nick@willhoft.co.uk>
2025-08-17 18:51:38 +0000

    Fix: export entries default

--- src/data/entries.js
+++ src/data/entries.js
@@ -4,24 +4,21 @@ const entries = [
     title: "Towering Balsam",
     excerpt: "IT’S BEEN QUITE A WHILE SINCE I WAS OUT with Stroud Valleys Project, so it was nice to catch up with friends...",
     image: "/my-blog-2/images/Ttoekbokki.jpg",
-    url: "/my-blog-2/posts/towering-balsam",
-    googleMapsUrl: "https://maps.app.goo.gl/udvM3eWHy65WAusj9"
+    url: "/my-blog-2/posts/towering-balsam"
   },
   {
     date: "THU 10TH JUL '25",
     title: "Freshford Harvest",
     excerpt: "MY FIRST SEED HARVEST OF THE SEASON with Glorious Grasslands was a long way down...",
     image: "/my-blog-2/images/Ttoekbokki.jpg",
-    url: "/my-blog-2/posts/freshford-harvest",
-    googleMapsUrl: "hhttps://maps.app.goo.gl/udvM3eWHy65WAusj9"
+    url: "/my-blog-2/posts/freshford-harvest"
   },
   {
     date: "WED 9TH JUL '25",
     title: "Heritage Heroes",
     excerpt: "A MID MORNING APPOINTMENT AT THE council chambers at Ebley where the students at the Star College were presented with their awards...",
     image: "/my-blog-2/images/Ttoekbokki.jpg",
-    url: "/my-blog-2/posts/heritage-heroes",
-    googleMapsUrl: "https://maps.app.goo.gl/udvM3eWHy65WAusj9"
+    url: "/my-blog-2/posts/heritage-heroes"
   }
 ];
 
