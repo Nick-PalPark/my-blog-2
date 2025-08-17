9596e5aab72ec415c9245ce1db5ab877e8342107
Nick-PalPark <nick@willhoft.co.uk>
2025-08-17 19:46:01 +0000

    help!

--- src/data/entries.js
+++ src/data/entries.js
@@ -2,23 +2,16 @@ const entries = [
   {
     date: "WED 23RD JUL '25",
     title: "Towering Balsam",
-    excerpt: "IT’S BEEN QUITE A WHILE SINCE I WAS OUT with Stroud Valleys Project, so it was nice to catch up with friends...",
+    excerpt: "IT’S BEEN QUITE A WHILE...",
     image: "/my-blog-2/images/Ttoekbokki.jpg",
     url: "/my-blog-2/posts/towering-balsam"
   },
   {
     date: "THU 10TH JUL '25",
     title: "Freshford Harvest",
-    excerpt: "MY FIRST SEED HARVEST OF THE SEASON with Glorious Grasslands was a long way down...",
+    excerpt: "MY FIRST SEED HARVEST...",
     image: "/my-blog-2/images/Ttoekbokki.jpg",
     url: "/my-blog-2/posts/freshford-harvest"
-  },
-  {
-    date: "WED 9TH JUL '25",
-    title: "Heritage Heroes",
-    excerpt: "A MID MORNING APPOINTMENT AT THE council chambers at Ebley where the students at the Star College were presented with their awards...",
-    image: "/my-blog-2/images/Ttoekbokki.jpg",
-    url: "/my-blog-2/posts/heritage-heroes"
   }
 ];
 
