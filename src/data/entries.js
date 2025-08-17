fc060a386eee5f6c7ab8e9627b7be4aed3faf0b0
Nick-PalPark <nick@willhoft.co.uk>
2025-08-17 18:16:09 +0000

    Fix: export entries default

--- src/data/entries.js
+++ src/data/entries.js
@@ -1,26 +1,28 @@
 const entries = [
   {
-    image: "/my-blog-2/images/Ttoekbokki.jpg",
     date: "WED 23RD JUL '25",
     title: "Towering Balsam",
     excerpt: "IT’S BEEN QUITE A WHILE SINCE I WAS OUT with Stroud Valleys Project, so it was nice to catch up with friends...",
-    url: "/my-blog-2/posts/towering-balsam"
+    image: "/my-blog-2/images/Ttoekbokki.jpg",
+    url: "/my-blog-2/posts/towering-balsam",
+    googleMapsUrl: "https://maps.app.goo.gl/udvM3eWHy65WAusj9"
   },
   {
-    image: "/my-blog-2/images/Ttoekbokki.jpg",
     date: "THU 10TH JUL '25",
     title: "Freshford Harvest",
-    excerpt: "MY FIRST SEED HARVEST OF THE SEASON with Glorious Grasslands was a long way down. At the very bottom of the Cotswolds...",
-    url: "/my-blog-2/posts/freshford-harvest"
+    excerpt: "MY FIRST SEED HARVEST OF THE SEASON with Glorious Grasslands was a long way down...",
+    image: "/my-blog-2/images/Ttoekbokki.jpg",
+    url: "/my-blog-2/posts/freshford-harvest",
+    googleMapsUrl: "hhttps://maps.app.goo.gl/udvM3eWHy65WAusj9"
   },
   {
-    image: "/my-blog-2/images/Ttoekbokki.jpg",
     date: "WED 9TH JUL '25",
     title: "Heritage Heroes",
     excerpt: "A MID MORNING APPOINTMENT AT THE council chambers at Ebley where the students at the Star College were presented with their awards...",
-    url: "/my-blog-2/posts/heritage-heroes"
+    image: "/my-blog-2/images/Ttoekbokki.jpg",
+    url: "/my-blog-2/posts/heritage-heroes",
+    googleMapsUrl: "https://maps.app.goo.gl/udvM3eWHy65WAusj9"
   }
 ];
 
 export default entries;
-
